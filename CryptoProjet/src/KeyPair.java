8a1bf15ed7859624e76dbb4c060a363a73f6fc40
yasmine <yasmine@OMEN>
2024-03-15 23:30:46 +0100

    INITIAL COMMIT

---
+++ CryptoProjet/src/KeyPair.java
@@ -0,0 +1,28 @@
+
+
+import it.unisa.dia.gas.jpbc.Element;
+
+/**
+ *
+ * @author imino
+ */
+public class KeyPair {
+   
+  public  String pk; //identité de l'utilisateur
+  private  Element sk; // clef privée de l'utilisateur
+
+    public KeyPair(String pk, Element sk) {
+        this.pk = pk;
+        this.sk = sk;
+    }
+
+    public String getPk() {
+        return pk;
+    }
+
+    public Element getSk() {
+        return sk;
+    }
+    
+    
+}
