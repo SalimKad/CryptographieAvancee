95ea0b45d988807fa9c057db4f80790f331a3ef6
yasel0 <132781455+yasel0@users.noreply.github.com>
2024-03-19 23:53:52 +0100

    IBEcipher.java
    updated

--- src/main/java/com/example/projet_crypto_v2/IBEcipher.java
+++ src/main/java/com/example/projet_crypto_v2/IBEcipher.java
@@ -1,32 +1,63 @@
-package com.example.projet_crypto_v2;
+import java.io.Serializable;
+
+
 
 import it.unisa.dia.gas.jpbc.Element;
 
-public class IBEcipher {
+
+
+public class IBEcipher implements Serializable {
+
     
-    private Element U; // rP (vu dans le cours)
+
+    private byte[] U; // rP (vu dans le cours)
+
     
+
     byte[] V; // K xor e(Q_id,P_pub) avec K la clef symmetrique AES
+
     
+
     byte[] Aescipher; // résultat du chiffrement avec AES
 
-    public IBEcipher(Element U, byte[] V, byte[] Aescipher) {
+
+
+    public IBEcipher(byte[] U, byte[] V, byte[] Aescipher) {
+
         this.U = U;
+
         this.V = V;
+
         this.Aescipher = Aescipher;
+
     }
+
+
 
     public byte[] getAescipher() {
+
         return Aescipher;
+
     }
 
-    public Element getU() {
+
+
+    public byte[] getU() {
+
         return U;
+
     }
+
+
 
     public byte[] getV() {
+
         return V;
+
     }
+
     
+
     
+
 }
